71bc0016fca96f638935fff0cd873ea98d84fb78
Sergey <pogrebnyak.vetal1996@gmail.com>
2021-12-15 14:27:41 +0200

    wip

--- ChelseabandSDK/Sources/Extensions/Observable.swift
+++ ChelseabandSDK/Sources/Extensions/Observable.swift
@@ -7,26 +7,6 @@
 
 import RxSwift
 
-extension Observable where Element == Data {
-
-    ///Filters hex value of data, filter if command starts with `hex` performs guard check for `byteIndex`
-    /// - Parameter hex: Hex value to check if data starts with this `hex`
-    /// - Parameter byteIndex: index of byte to determin wether chain could be completed
-    /// - Parameter mininumBytes: guard check
-    /// - Returns: completed observable`Observable<Void> `.
-    public func completeWhenByteEqualsToOne(hexStartWith hex: String, byteIndex: Int = 3) -> Observable<Element> {
-        filter {
-            $0.hex.starts(with: hex) && $0.bytes.count >= byteIndex + 1
-        }.map { data -> Data in
-            guard data.bytes[byteIndex] == 1 else {
-                throw CommandError.invalid
-            }
-
-            return data
-        }.take(1)//NOTE: Wee need to complete observable sequence
-    }
-}
-
 extension ObservableType {
     func mapTimeoutError(to error: Error) -> Observable<Element> {
         catchError {
